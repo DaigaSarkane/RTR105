73de69b97895c6e175e0a813f66b230ab68ab598
user <daiga.sarkane1@gmail.com>
2018-10-01 15:28:32 +0300

    20181001_15_30

---
+++ test_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+a = 10
+b = 1.89
+c = 'D'
+# http://www.cplusplus.com/referance/cstdio/printf/
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+
+a = 56
+b = 8.96
+c = 'F'
+print("a = %d"%(a))
+print("b = %.10f"%(b))
+print("c = %s"%(c))
+
